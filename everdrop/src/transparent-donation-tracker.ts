c6b3c0c45388217d346c5c826fb49fadc6e6fa87
Aryan Kumar <aryankumar877@gmail.com>
2024-11-14 23:31:25 +0530

    Initialize subgraph

---
+++ everdrop/src/transparent-donation-tracker.ts
@@ -0,0 +1,212 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BadgeEarned as BadgeEarnedEvent,
+  CauseCreated as CauseCreatedEvent,
+  CauseTargetReached as CauseTargetReachedEvent,
+  DonationReceived as DonationReceivedEvent,
+  FundsWithdrawn as FundsWithdrawnEvent,
+  ImpactScoreUpdated as ImpactScoreUpdatedEvent,
+  MilestoneAdded as MilestoneAddedEvent,
+  MilestoneCompleted as MilestoneCompletedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/TransparentDonationTracker/TransparentDonationTracker"
+import {
+  Approval,
+  ApprovalForAll,
+  BadgeEarned,
+  CauseCreated,
+  CauseTargetReached,
+  DonationReceived,
+  FundsWithdrawn,
+  ImpactScoreUpdated,
+  MilestoneAdded,
+  MilestoneCompleted,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBadgeEarned(event: BadgeEarnedEvent): void {
+  let entity = new BadgeEarned(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.donor = event.params.donor
+  entity.badgeType = event.params.badgeType
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleCauseCreated(event: CauseCreatedEvent): void {
+  let entity = new CauseCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.causeName = event.params.causeName
+  entity.description = event.params.description
+  entity.beneficiary = event.params.beneficiary
+  entity.targetAmount = event.params.targetAmount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleCauseTargetReached(event: CauseTargetReachedEvent): void {
+  let entity = new CauseTargetReached(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.cause = event.params.cause
+  entity.finalAmount = event.params.finalAmount
+  entity.donorCount = event.params.donorCount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDonationReceived(event: DonationReceivedEvent): void {
+  let entity = new DonationReceived(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.donor = event.params.donor
+  entity.cause = event.params.cause
+  entity.amount = event.params.amount
+  entity.impactScore = event.params.impactScore
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleFundsWithdrawn(event: FundsWithdrawnEvent): void {
+  let entity = new FundsWithdrawn(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.cause = event.params.cause
+  entity.beneficiary = event.params.beneficiary
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleImpactScoreUpdated(event: ImpactScoreUpdatedEvent): void {
+  let entity = new ImpactScoreUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.donor = event.params.donor
+  entity.newScore = event.params.newScore
+  entity.cause = event.params.cause
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMilestoneAdded(event: MilestoneAddedEvent): void {
+  let entity = new MilestoneAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.cause = event.params.cause
+  entity.description = event.params.description
+  entity.targetAmount = event.params.targetAmount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMilestoneCompleted(event: MilestoneCompletedEvent): void {
+  let entity = new MilestoneCompleted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.cause = event.params.cause
+  entity.milestoneIndex = event.params.milestoneIndex
+  entity.completionTime = event.params.completionTime
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ everdrop/tests/transparent-donation-tracker-utils.ts
@@ -0,0 +1,341 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  BadgeEarned,
+  CauseCreated,
+  CauseTargetReached,
+  DonationReceived,
+  FundsWithdrawn,
+  ImpactScoreUpdated,
+  MilestoneAdded,
+  MilestoneCompleted,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/TransparentDonationTracker/TransparentDonationTracker"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBadgeEarnedEvent(
+  donor: Address,
+  badgeType: string,
+  tokenId: BigInt
+): BadgeEarned {
+  let badgeEarnedEvent = changetype<BadgeEarned>(newMockEvent())
+
+  badgeEarnedEvent.parameters = new Array()
+
+  badgeEarnedEvent.parameters.push(
+    new ethereum.EventParam("donor", ethereum.Value.fromAddress(donor))
+  )
+  badgeEarnedEvent.parameters.push(
+    new ethereum.EventParam("badgeType", ethereum.Value.fromString(badgeType))
+  )
+  badgeEarnedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return badgeEarnedEvent
+}
+
+export function createCauseCreatedEvent(
+  causeName: string,
+  description: string,
+  beneficiary: Address,
+  targetAmount: BigInt
+): CauseCreated {
+  let causeCreatedEvent = changetype<CauseCreated>(newMockEvent())
+
+  causeCreatedEvent.parameters = new Array()
+
+  causeCreatedEvent.parameters.push(
+    new ethereum.EventParam("causeName", ethereum.Value.fromString(causeName))
+  )
+  causeCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "description",
+      ethereum.Value.fromString(description)
+    )
+  )
+  causeCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "beneficiary",
+      ethereum.Value.fromAddress(beneficiary)
+    )
+  )
+  causeCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "targetAmount",
+      ethereum.Value.fromUnsignedBigInt(targetAmount)
+    )
+  )
+
+  return causeCreatedEvent
+}
+
+export function createCauseTargetReachedEvent(
+  cause: string,
+  finalAmount: BigInt,
+  donorCount: BigInt
+): CauseTargetReached {
+  let causeTargetReachedEvent = changetype<CauseTargetReached>(newMockEvent())
+
+  causeTargetReachedEvent.parameters = new Array()
+
+  causeTargetReachedEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+  causeTargetReachedEvent.parameters.push(
+    new ethereum.EventParam(
+      "finalAmount",
+      ethereum.Value.fromUnsignedBigInt(finalAmount)
+    )
+  )
+  causeTargetReachedEvent.parameters.push(
+    new ethereum.EventParam(
+      "donorCount",
+      ethereum.Value.fromUnsignedBigInt(donorCount)
+    )
+  )
+
+  return causeTargetReachedEvent
+}
+
+export function createDonationReceivedEvent(
+  donor: Address,
+  cause: string,
+  amount: BigInt,
+  impactScore: BigInt,
+  timestamp: BigInt
+): DonationReceived {
+  let donationReceivedEvent = changetype<DonationReceived>(newMockEvent())
+
+  donationReceivedEvent.parameters = new Array()
+
+  donationReceivedEvent.parameters.push(
+    new ethereum.EventParam("donor", ethereum.Value.fromAddress(donor))
+  )
+  donationReceivedEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+  donationReceivedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+  donationReceivedEvent.parameters.push(
+    new ethereum.EventParam(
+      "impactScore",
+      ethereum.Value.fromUnsignedBigInt(impactScore)
+    )
+  )
+  donationReceivedEvent.parameters.push(
+    new ethereum.EventParam(
+      "timestamp",
+      ethereum.Value.fromUnsignedBigInt(timestamp)
+    )
+  )
+
+  return donationReceivedEvent
+}
+
+export function createFundsWithdrawnEvent(
+  cause: string,
+  beneficiary: Address,
+  amount: BigInt
+): FundsWithdrawn {
+  let fundsWithdrawnEvent = changetype<FundsWithdrawn>(newMockEvent())
+
+  fundsWithdrawnEvent.parameters = new Array()
+
+  fundsWithdrawnEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+  fundsWithdrawnEvent.parameters.push(
+    new ethereum.EventParam(
+      "beneficiary",
+      ethereum.Value.fromAddress(beneficiary)
+    )
+  )
+  fundsWithdrawnEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return fundsWithdrawnEvent
+}
+
+export function createImpactScoreUpdatedEvent(
+  donor: Address,
+  newScore: BigInt,
+  cause: string
+): ImpactScoreUpdated {
+  let impactScoreUpdatedEvent = changetype<ImpactScoreUpdated>(newMockEvent())
+
+  impactScoreUpdatedEvent.parameters = new Array()
+
+  impactScoreUpdatedEvent.parameters.push(
+    new ethereum.EventParam("donor", ethereum.Value.fromAddress(donor))
+  )
+  impactScoreUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newScore",
+      ethereum.Value.fromUnsignedBigInt(newScore)
+    )
+  )
+  impactScoreUpdatedEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+
+  return impactScoreUpdatedEvent
+}
+
+export function createMilestoneAddedEvent(
+  cause: string,
+  description: string,
+  targetAmount: BigInt
+): MilestoneAdded {
+  let milestoneAddedEvent = changetype<MilestoneAdded>(newMockEvent())
+
+  milestoneAddedEvent.parameters = new Array()
+
+  milestoneAddedEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+  milestoneAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "description",
+      ethereum.Value.fromString(description)
+    )
+  )
+  milestoneAddedEvent.parameters.push(
+    new ethereum.EventParam(
+      "targetAmount",
+      ethereum.Value.fromUnsignedBigInt(targetAmount)
+    )
+  )
+
+  return milestoneAddedEvent
+}
+
+export function createMilestoneCompletedEvent(
+  cause: string,
+  milestoneIndex: BigInt,
+  completionTime: BigInt
+): MilestoneCompleted {
+  let milestoneCompletedEvent = changetype<MilestoneCompleted>(newMockEvent())
+
+  milestoneCompletedEvent.parameters = new Array()
+
+  milestoneCompletedEvent.parameters.push(
+    new ethereum.EventParam("cause", ethereum.Value.fromString(cause))
+  )
+  milestoneCompletedEvent.parameters.push(
+    new ethereum.EventParam(
+      "milestoneIndex",
+      ethereum.Value.fromUnsignedBigInt(milestoneIndex)
+    )
+  )
+  milestoneCompletedEvent.parameters.push(
+    new ethereum.EventParam(
+      "completionTime",
+      ethereum.Value.fromUnsignedBigInt(completionTime)
+    )
+  )
+
+  return milestoneCompletedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
